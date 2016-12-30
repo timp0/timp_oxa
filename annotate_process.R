b8cff71d3ef0ef53a9202896b214f374441ff874
Winston <wtimp@jhu.edu>
2016-12-30 13:36:57 -0500

    Annotation work

---
+++ annotate_process.R
@@ -0,0 +1,93 @@
+library(tidyverse)
+
+rawdir="/atium/Data/Nanopore/Analysis/161223_kpneumo"
+workdir="/atium/Data/Nanopore/Analysis/161223_kpneumo/"
+outdir="~/Dropbox/Data/Nanopore/161222_oxamut"
+blast.cols=c("query.id", "subject.id", "per.identity", "align.len", "mismatch", "gap.opens", "q.start", "q.end", "s.start", "s.end", "evalue", "bit.score")
+
+card.dir="/atium/Data/Reference/CARD/"
+
+card.tab=read_csv(file.path(card.dir, "aro.csv"))
+
+setwd(workdir)
+
+##Load CSV with sample info
+
+dataloc=read_csv(file.path(outdir, "dataloc.csv")) %>%
+    filter(Sequenced=="both")
+
+for (i in 1:dim(dataloc)[1]) {
+
+    samp=dataloc$trish.id[i]
+    
+    ##Run Prokka
+    system(paste0("~/Code/prokka/bin/prokka --outdir ", workdir, samp, "_genus --genus Klebsiella --usegenus --prefix ", samp, "_genus ", dataloc$canu.assembly[i]))
+
+    file.copy(file.path(workdir, paste0(samp, "_genus"), paste0(samp, "_genus.gff")), file.path(outdir, "annot", paste0(samp, "_genus.gff")))
+    
+    ##Run RGI (CARD)
+    system(paste0("python ~/Code/rgi_card/rgi.py -t contig -i ", dataloc$canu.assembly[i], " -n 8 -o ", samp, ".rgicard"))
+    system(paste0("python ~/Code/rgi_card/convertJsonToTSV.py -i ", samp, ".rgicard.json -o ", samp, ".rgicard"))
+    
+    rgi=read_tsv(file.path(workdir, paste0(samp, ".rgicard.txt"))) %>%
+        mutate(clabel=sapply(strsplit(ORF_ID, split="_"), function(x) {x[1]})) %>%
+        mutate(attrib=paste0("Name=",ARO_name, ";gene=", ARO_name, ";ARO_category=", AR0_category, ";cutoff=", CUT_OFF))
+    
+    rgi.gff=rgi %>%
+        mutate(source="rgicard", feature="CDS", frame="0") %>%
+        select(seqname=clabel, source=source, feature=feature, start=START,
+               end=STOP, score=Best_Identities, strand=ORIENTATION,
+               frame=frame, attribute=attrib) 
+    
+    write_tsv(rgi.gff, file.path(outdir, "annot", paste0(samp, ".rgicard.gff")), col_names=F)
+    
+    
+    ##Run CARD Blast
+    system(paste0("blastn -db /atium/Data/Reference/CARD/CARDblast -query ", dataloc$canu.assembly[i], " -evalue 1e-50 -outfmt 6 -best_hit_overhang .1 -best_hit_score_edge 1e-5 >", workdir, samp, ".cardblast.tsv"))
+    
+    card.res=read_tsv(file.path(workdir, paste0(samp, ".cardblast.tsv")), col_names=blast.cols) %>%
+        mutate(aro=sapply(strsplit(subject.id, split="\\|"), function(x) {x[5]})) %>%
+        mutate(aro.idx=pmatch(aro, card.tab$Accession)) %>%
+        mutate(aro.name=card.tab$Name[aro.idx]) %>%
+        mutate(aro.desc=card.tab$Description[aro.idx])   
+    
+    card.gff=card.res %>%
+        mutate(source="cardblast", feature="CDS", frame="0") %>%
+        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+        mutate(attrib=paste0("Name=", aro.name, ";gene=", aro.name, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue, ";ARO=",aro, ";Description=",aro.desc)) %>%
+        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+
+    write_tsv(card.gff, file.path(outdir, "annot", paste0(samp, ".cardblast.gff")), col_names=F)
+    
+    
+    
+    ##Look for specific gene mutations
+    mutgenedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/mutdbase"
+    system(paste0("blastn -db ", mutgenedb, " -query ", dataloc$canu.assembly[i], " -outfmt 6 >", workdir, samp, ".mutblast.tsv"))
+    
+    mut.res=read_tsv(file.path(workdir, paste0(samp, ".mutblast.tsv")), col_names=blast.cols)
+    
+    mut.gff=mut.res %>%
+        mutate(source="mutblast", feature="CDS", frame="0") %>%
+        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+        mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
+        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+    
+    write_tsv(mut.gff, file.path(outdir, "annot", paste0(samp, ".mutblast.gff")), col_names=F)
+    
+    
+    ##look for specific is recog sequences
+    isedb="/atium/Data/Nanopore/Analysis/161223_kpneumo/genedb/isecp1blast"
+    system(paste0("blastn -task blastn-short -db ", isedb, " -query ", dataloc$canu.assembly[i], " -outfmt 6 >", workdir, samp, ".iseblast.tsv"))
+    
+    ise.res=read_tsv(file.path(workdir, paste0(samp, ".iseblast.tsv")), col_names=blast.cols)
+    
+    ise.gff=ise.res %>%
+        mutate(source="iseblast", feature="Mobile_element", frame="0") %>%
+        mutate(strand=ifelse(s.end>s.start, yes="+", no="-")) %>%
+        mutate(attrib=paste0("Name=", subject.id, ";gene=", subject.id, ";per.identity=", per.identity, ";bit.score=", bit.score,";evalue=",evalue)) %>%
+        select(seqname=query.id, source=source, feature=feature, start=q.start, end=q.end, score=bit.score, strand=strand, frame=frame, attribute=attrib)
+    
+    write_tsv(ise.gff, file.path(outdir, paste0(samp, ".iseblast.gff")), col_names=F)
+    
+}
--- prokka.sh
+++ prokka.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-#command I used for prokka (example with 10A using Illumina)
 
-/home/shao4/prokka/bin/prokka --outdir /atium/Data/NGS/Aligned/161027_SSSSS/SPAdes/assemblies/10A_genus --genus Klebsiella --usegenus --addgenes --prefix 10A_genus /atium/Data/NGS/Aligned/161027_SSSSS/SPAdes/spade_contigs/Illumina/10A_contigs.fasta
-
-#note that on some of them I added the --addgenes flag - I didn't find it very helpful, as it seemed to just duplicate your results
-
-#with nanopore
-/home/shao4/prokka/bin/prokka --outdir /atium/Data/NGS/Aligned/161027_SSSSS/SPAdes/assemblies/1A_nanopore_genus --genus Klebsiella --usegenus --prefix 1A_nanopore /atium/Data/NGS/Aligned/161027_SSSSS/SPAdes/spade_contigs/nanopore_Illumina/Nanopore/161109_SSSSS_1A.nanopore.contigs.fasta
+##From Steph (altered by WT)
+##Making K.pneumo dbase
+~/Code/prokka/bin/prokka-genbank_to_fasta_db /atium/Data/NGS/Aligned/161027_SSSSS/SSSSS_ref/*.gbk > Klebsiella.faa
 
 #building the custom database for Klebsiella (notes also in evernote):
 # first, download all of the genbank files from NCBI for various K. pneumo strains - I have them downloaded at /atium/Data/NGS/Aligned/161027_SSSSS/SSSSS_ref/
 #follow instructions as listed on website
 
-/home/shao4/prokka/bin/prokka-genbank_to_fasta_db *.gbk > Klebsiella.faa
-cd-hit -i Klebsiella.faa -o Klebsiella -T 0 -M 0 -g 1 -s 0.8 -c 0.9
+cd-hit -i Klebsiella.faa -o Klebsiella -T 8 -M 24000 -g 1 -s 0.8 -c 0.9
 rm -fv Klebsiella.clstr Klebsiella.faa
 makeblastdb -dbtype prot -in Klebsiella
-mv Klebsiella.p* /home/shao4/prokka/db/genus
+mv Klebsiella.p* ~/Code/prokka/db/genus
+
+
+##From Steph (altered by WT)
+#command I used for prokka 
+label=811A
+
+~/Code/prokka/bin/prokka --outdir /atium/Data/Nanopore/Analysis/161223_kpneumo/${label}_genus --genus Klebsiella --usegenus \
+    --prefix 811A_genus /atium/Data/NGS/Aligned/161027_SSSSS/SPAdes/spade_contigs/nanopore_Illumina/Nanopore/811A.nanopore.contigs.fasta
+
+#note that on some of them I added the --addgenes flag - I didn't find it very helpful, as it seemed to just duplicate your results
+
 
 
 
