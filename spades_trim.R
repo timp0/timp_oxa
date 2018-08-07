fa7af35a2fd83b64be538451a64acce12f2fa54d
Winston <wtimp@jhu.edu>
2018-08-06 21:22:56 -0400

    spades trimmomatic

--- spades_trim.R
+++ spades_trim.R
@@ -15,20 +15,29 @@ if (!dir.exists(workdir)) {
     dir.create(workdir)
 }
 
-if (!dir.exists(outdir)) {
-    dir.create(outdir)
-}
-
 
 setwd(workdir)
 
-for (i in 1:1) {
-##Fix here
-    system(paste0("atropos --aligner insert -T 6 -a CTGTCTCTTATACACATCT ",
-                  "-A CTGTCTCTTATACACATCT ",
-                  "-q 20 -o /tmp/read1.trim.fq.gz -p /tmp/read2.trim.fq.gz ",
-                  "-pe1 ",dataloc$illumina.r1[i]," -pe2 ", dataloc$illumina.r2[i]))
+for (i in c(3, 5, 11)) {
+    ##Fix here
+    ##tried TCGTCGGCAGCGTC but still contaminated.  Trying this other one now
+    file.remove("/tmp/read1.trim.fq.gz")
+    file.remove("/tmp/read2.trim.fq.gz")
+    #system(paste0("atropos -T 6 -a GTCTCGTGGGCTCGG ",
+    #              "-A GTCTCGTGGGCTCGG ",
+    #              "-q 20 -o /tmp/read1.trim.fq.gz -p /tmp/read2.trim.fq.gz ",
+    #              "-pe1 ",dataloc$illumina.r1[i]," -pe2 ", dataloc$illumina.r2[i]))
+
+    system(paste0("java -jar ~/Code/Trimmomatic-0.38/trimmomatic-0.38.jar PE -phred33 -threads 6 ",
+                  dataloc$illumina.r1[i], " ", dataloc$illumina.r2[i],
+                  " /tmp/read1.trim.fq.gz /tmp/read1.unpaired.fq.gz",
+                  " /tmp/read2.trim.fq.gz /tmp/read2.unpaired.fq.gz",
+                  " ILLUMINACLIP:/home/timp/Code/Trimmomatic-0.38/adapters/NexteraPE-PE.fa:2:30:10",
+                  " LEADING:20 TRAILING:20 SLIDINGWINDOW:4:20 MINLEN:40"))
+                  
+
     
     system(paste0("spades.py -1 /tmp/read1.trim.fq.gz -2 /tmp/read2.trim.fq.gz ",
                   " -o ", dataloc$trish.id[i], "/spades"))
 }
+ 
